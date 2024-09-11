3e5c3892832e404e1a3ab2083b2bd5294fe5d586
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-09-11 21:25:08 +0600

    added handleAddSpots

--- src/components/AddTouristSpots.jsx
+++ src/components/AddTouristSpots.jsx
@@ -1,7 +1,26 @@
 
 
 const AddTouristSpots = () => {
-    
+    const handleAddSpots = e => {
+        e.preventDefault();
+        const form = e.target;
+        const tourists_spot_name = form.tourists_spot_name.value;
+        const country_Name = form.country_Name.value;
+        const location = form.location.value;
+        const average_cost = form.average_cost.value;
+        const seasonality = form.seasonality.value;
+        const short_description = form.short_description.value;
+        const travel_time = form.travel_time.value;
+        const totalVisitorsPerYear = form.totalVisitorsPerYear.value;
+        const user_email = form.user_email.value;
+        const user_Name = form.user_Name.value;
+        const photo = form.photo.value;
+        const newSpot = {
+            tourists_spot_name, country_Name, location, average_cost, seasonality, short_description, travel_time, totalVisitorsPerYear, user_email, user_Name, photo 
+        };
+        console.log(newSpot);
+
+    }
     return (
         <div>
 
@@ -12,7 +31,7 @@ const AddTouristSpots = () => {
                         <div className='px-[25px] md:px-[40px] lg:px-[60px] xl:px-[110px]'>
                             <h3 className='font-quicksand font-bold text-[28px] md:text-[35px] lg:text-[45px] text-[#374151] pt-[20px] md:pt-[30px] lg:pt-[40px] text-center'>Add Tourists Spot</h3>
                             <p className='font-openSans text-[12px] md:text-[14px] lg:text-[17px] text-[#1B1A1AB3] mt-2 md:mt-3 lg:mt-6 text-center px-2 lg:px-20 mb-4 md:mb-5 lg:mb-8'>TourNexus connects you to the best tourist spots, offering tailored adventures, stunning locations, and unforgettable travel experiences.</p>
-                            <form className='font-raleway'>
+                            <form onSubmit={handleAddSpots} className='font-raleway'>
                                 <div className='md:flex md:gap-4 lg:gap-6'>
                                     <div className="form-control font-openSans w-full">
                                         <label className="label">
@@ -47,7 +66,7 @@ const AddTouristSpots = () => {
                                             <span className="label-text text-base md:text-lg lg:text-xl font-openSans font-semibold text-[#1B1A1ACC]">Average Cost
                                             </span>
                                         </label>
-                                        <input type="number" name='average_cost'
+                                        <input type="text" name='average_cost'
                                             placeholder="Enter average cost"
                                             className="input input-bordered w-full text-[14px] md:text-[16px]" required />
                                     </div>
@@ -57,8 +76,8 @@ const AddTouristSpots = () => {
                                         <label className="label">
                                             <span className="label-text text-base md:text-lg lg:text-xl font-openSans font-semibold text-[#1B1A1ACC]">Seasonality</span>
                                         </label>
-                                        <select name="country_Name" className="select select-bordered w-full text-[14px] md:text-[16px]">
-                                            <option disabled selected>Select a Season</option>
+                                        <select  name="seasonality" className="select select-bordered w-full text-[14px] md:text-[16px]">
+                                            <option  disabled selected>Select a Season</option>
                                             <option>Summer</option>
                                             <option>Winter</option>
                                         </select>

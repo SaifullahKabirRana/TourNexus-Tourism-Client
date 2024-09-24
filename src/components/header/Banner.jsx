7324b4f3a8a5134b69dd59e5424801e081db148c
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-09-25 02:32:25 +0600

    added React Simple Typewriter & react-awesome-reveal

--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -4,65 +4,75 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import './swiper.css'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
     return (
         <div className="mt-4 md:mt-5 lg:mt-6">
             <Swiper
-                 spaceBetween={30}
-                 centeredSlides={true}
-                 autoplay={{
-                     delay: 2000,
-                     disableOnInteraction: false,
-                 }}
-                 pagination={{
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
                     clickable: true,
                 }}
-                 navigation={true}
-                 modules={[Autoplay, Pagination, Navigation]}
-                 loop={true}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                loop={true}
             >
                 {/* banner-1 */}
-                <SwiperSlide> 
+                <SwiperSlide>
                     <div className="banner-4 rounded-badge bg-no-repeat bg-cover bg-top min-h-[35vh] md:min-h-[60vh] lg:min-h-[70vh] xl:min-h-[80vh]">
-                <div className="py-16 md:py-28 lg:py-44 xl:py-52 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
-                    <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold">Explore The Worlds</h2>
-                    <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
-                    <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
-                </div>
-            </div>
-            </SwiperSlide>
-                 {/* banner-2 */}
+                        <div className="py-12 md:py-28 lg:py-44 xl:py-52 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
+                            <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold">
+
+                                <Typewriter
+                                    words={['Explore The Worlds', 'Explore Dream Destinations']}
+                                    delaySpeed={400}
+                                    typeSpeed={60}
+                                >
+
+                                </Typewriter>
+                            </h2>
+                            <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
+                            <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* banner-2 */}
                 <SwiperSlide><div className="banner-2 rounded-badge bg-no-repeat bg-cover bg-top min-h-[35vh] md:min-h-[60vh] lg:min-h-[70vh] xl:min-h-[80vh]">
-                <div className="py-16 md:py-28 lg:py-44 xl:py-52 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
-                    <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold">Explore The Worlds</h2>
-                    <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
-                    <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
+                    <div className="py-12 md:py-28 lg:py-44 xl:py-52 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
+                        <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold">Explore Dream Destinations</h2>
+                        <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
+                        <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
+                    </div>
                 </div>
-            </div>
-            </SwiperSlide>
-            {/* banner-3 */}
-                <SwiperSlide> 
+                </SwiperSlide>
+                {/* banner-3 */}
+                <SwiperSlide>
                     <div className="banner-3 rounded-badge bg-no-repeat bg-cover bg-top min-h-[35vh] md:min-h-[60vh] lg:min-h-[70vh] xl:min-h-[80vh]">
-                <div className="py-12 md:py-28 lg:py-36 xl:py-44 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
-                    <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold md:w-[75%] xl:w-[60%]">Tour Travel & Adventure Camping</h2>
-                    <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
-                    <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
-                </div>
-            </div>
-            </SwiperSlide>
-            {/* banner-4 */}
+                        <div className="py-12 md:py-28 lg:py-36 xl:py-44 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
+                            <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold md:w-[75%] xl:w-[60%]">Tour Travel & Adventure Camping</h2>
+                            <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
+                            <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                {/* banner-4 */}
                 <SwiperSlide>
                     <div className="banner-1 rounded-badge bg-no-repeat bg-cover bg-top min-h-[35vh] md:min-h-[60vh] lg:min-h-[70vh] xl:min-h-[80vh]">
-                <div className="py-16 md:py-28 lg:py-44 xl:py-52 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
-                    <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold">Explore The Worlds</h2>
-                    <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
-                    <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
-                </div>
-            </div>
-            </SwiperSlide>
+                        <div className="py-12 md:py-28 lg:py-44 xl:py-52 pl-8 md:pl-16 lg:pl-20 xl:pl-28">
+                            <h2 className="font-quicksand text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-white font-extrabold">Travel Made Simple</h2>
+                            <p className="font-openSans text-white text-sm md:text-lg lg:text-xl xl:text-2xl py-2 md:py-3 lg:py-4">People Don’t Take, Trips Take People</p>
+                            <button className="btn btn-sm md:btn-md border-none bg-[#56b63e] text-white text-sm md:text-lg md:px-6 font-quicksand mt-1 md:mt-2 lg:mt-4">Let’s Get Started</button>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
-            
+
         </div>
     );
 };
--- src/components/touristSpots/TouristSpots.jsx
+++ src/components/touristSpots/TouristSpots.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+
 import { useLoaderData } from "react-router-dom";
 import TouristSpot from "./TouristSpot";
+import { Fade,Slide } from "react-awesome-reveal";
 
 const TouristSpots = () => {
     const touristSpots = useLoaderData();
@@ -8,8 +9,12 @@ const TouristSpots = () => {
     return (
         <div>
             <div className="mt-8 md:mt-12 lg:mt-14 xl:mt-16">
+                <Slide>
                 <h2 className="text-center font-quicksand text-2xl md:text-3xl lg:text-4xl font-bold">Tourists Spots</h2>
+                </Slide>
+                <Fade delay={700} cascade >
                 <p className="text-center w-[90%] md:w-2/3 lg:w-2/3 xl:w-2/4 mx-auto text-xs md:text-sm lg:text-base font-openSans font-light mt-2 md:mt-3 lg:mt-4">Explore top tourist spots around the world, showcasing stunning landscapes, iconic landmarks, and vibrant cultural attractions</p>
+                </Fade>
                 <div className="grid lg:gap-y-3 md:grid-cols-2 lg:grid-cols-3  md:gap-x-5 lg:gap-x-4 xl:gap-x-7 mt-[20px] md:mt-[30px] lg:mt-[45px] mx-2 md:mx-4 2xl:mx-10">
                     {
                         touristSpots.slice(0,6).map(touristSpot => <TouristSpot

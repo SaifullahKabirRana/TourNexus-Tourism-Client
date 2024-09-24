924e6430251652849742fb1e0c6c9b0821861e3f
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-09-25 01:31:18 +0600

    added loader in the categories page

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,13 +1,18 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Category from "./Category";
+import { AuthContext } from "../../providers/AuthProvider";
 
 
 const Categories = () => {
     const categories = useLoaderData();
+    const { loader } = useContext(AuthContext);
+    if (loader) {
+        return <div className='flex justify-center mt-[250px] md:mt-[350px]'>
+            <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-black"></div>
+        </div>
+    }
     
-    
-   
     return (
         <div className="mt-4 md:mt-6 lg:mt-8">
             <h2 className="text-center font-quicksand text-2xl md:text-3xl lg:text-4xl font-bold">Countries Tourist Spots </h2>
